f39dda3d50a3909233c76f12571f0af7249df701
gowthamvetri <gowthamvetriii@gmail.com>
2026-06-02 18:14:00 +0530

    email bugs fixed and implemented nodemailer insted of resend fix - 1

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,7 +1,11 @@
 import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
